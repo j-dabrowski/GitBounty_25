09764ccba38b4e7de3f3ae70e928fa76540ef559
Josef <josef.dabrowski@gmail.com>
2026-02-07 16:40:49 +0800

    closes #7, improves bounty rows and more info in bounty snapshot

--- frontend/constants.js
+++ frontend/constants.js
@@ -18,7 +18,7 @@ export const READ_RPC_URLS = [
 
 export const API_ORIGIN = "http://localhost:3000";
 
-export const factoryAddress = "0xf6C968e06747B92881208c45285A7Cd2689A7a54";
+export const factoryAddress = "0x5daD63916C4e6F2322F77621dbb61E015F467Aeb";
 
 export const factoryAbi = [
   "function bountyCount() view returns (uint256)",
@@ -28,18 +28,39 @@ export const factoryAbi = [
 ];
 
 export const bountyAbi = [
-  `function getBountySnapshot() view returns (
-      uint8 r_state,
-      address r_owner,
-      address r_factory,
-      bool r_initialized,
-      string r_repoOwner,
-      string r_repo,
-      string r_issueNumber,
-      uint256 r_totalFunding,
-      uint256 r_funderCount,
-      address r_lastWinner,
-      string r_lastWinnerUser,
-      uint256 r_lastBountyAmount
-  )`
+  {
+    "inputs": [],
+    "name": "getBountySnapshot",
+    "outputs": [
+      {
+        "internalType": "struct BountySnapshot",
+        "name": "s",
+        "type": "tuple",
+        "components": [
+          { "internalType": "enum GitbountyState", "name": "state", "type": "uint8" },
+          { "internalType": "address", "name": "owner", "type": "address" },
+          { "internalType": "address", "name": "factory", "type": "address" },
+          { "internalType": "bool", "name": "initialized", "type": "bool" },
+
+          { "internalType": "string", "name": "repoOwner", "type": "string" },
+          { "internalType": "string", "name": "repo", "type": "string" },
+          { "internalType": "string", "name": "issueNumber", "type": "string" },
+
+          { "internalType": "uint256", "name": "totalFunding", "type": "uint256" },
+          { "internalType": "uint256", "name": "funderCount", "type": "uint256" },
+
+          { "internalType": "address", "name": "lastWinner", "type": "address" },
+          { "internalType": "string", "name": "lastWinnerUser", "type": "string" },
+
+          { "internalType": "string", "name": "last_repo_owner", "type": "string" },
+          { "internalType": "string", "name": "last_repo", "type": "string" },
+          { "internalType": "string", "name": "last_issueNumber", "type": "string" },
+
+          { "internalType": "uint256", "name": "lastBountyAmount", "type": "uint256" }
+        ]
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
 ];
